da61160fcaeede6e3f3f0063fd9c59826134b6f0
Serhat GENÇ <gncsrht05@gmail.com>
2025-02-24 10:44:18 +0300

    modal kapatma eklendi.

---
+++ main.js
@@ -0,0 +1,7 @@
+const modalBtn = document.querySelector(".modal-btn-close");
+const modalWindow = document.querySelector(".modal");
+
+
+modalBtn.addEventListener("click", function() {
+   modalWindow.style.display="none";
+});
